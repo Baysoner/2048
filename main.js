ae8d40e3e9423252000081e9ad05c8417ffd7d8a
Artem Solovyov <artemsolovev121@gmail.com>
2024-08-05 12:21:32 +0500

    feat: add basic layout via js

--- main.js
+++ main.js
@@ -1,24 +1,9 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+import Grid from "/Grid.js";
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+document.querySelector("#app").innerHTML = `
+  <div id="game-board"></div>
+`;
 
-setupCounter(document.querySelector('#counter'))
+const gameBoard = document.getElementById("game-board");
+
+const grid = new Grid(gameBoard);
